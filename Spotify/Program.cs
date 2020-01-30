b7f0332624de6ac82dc8d55567e07e1210f370cd
kristian Vincentzen <kvincentzen@gmail.com>
2020-01-30 11:48:37 +0100

    fix

--- Spotify/Program.cs
+++ Spotify/Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
@@ -14,7 +13,7 @@ namespace Spotify
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).UseWebRoot("Content").Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
