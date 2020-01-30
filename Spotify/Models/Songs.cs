38f7baa39347c34d163a0d8d82570912d4c8cbf7
kristian Vincentzen <kvincentzen@gmail.com>
2020-01-30 14:58:45 +0100

    adad

---
+++ Spotify/Models/Songs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spotify.Models
+{
+    public class Songs
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int TrackId { get; set; }
+        public virtual Artists Artists { get; set; }
+
+    }
+}
--- Spotify/Startup.cs
+++ Spotify/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Spotify.DAL;
 
 namespace Spotify
 {
@@ -20,6 +22,8 @@ namespace Spotify
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = @"Server=DESKTOP-FRAVFCG; Database=PlayersPlayer; Trusted_Connection=True;";// User Id=sa;Password=Password!";
+            services.AddDbContext<PlayerContext>(g => g.UseSqlServer(connectionString));
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
